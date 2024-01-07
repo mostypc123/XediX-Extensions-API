da6ec978812cbca5305728efa17410d39959f8a8
Juraj K <140317448+mostypc123@users.noreply.github.com>
2024-01-07 12:54:47 +0100

    Update api.py

--- api.py
+++ api.py
@@ -21,3 +21,11 @@ import xedix
 def sh_add(cdgext, name):
     if name in xedix.text.get(2.0):
         Percolator(xedix.text).insertfilter(cdgext)
+def window_add(scrname):
+            window = tk.Tk(screenName = "XediX Extension: "+scrname)
+            window.pack()
+def menu_add_mainwindow(name):
+    xedix.root.config(menu=xedix.menu)
+    new_menu = tk.Menu(master = xedix.root, name=name)
+    def menu_mainwindow_add_command(command, label):
+        new_menu.add_command(label=label, command=command)
